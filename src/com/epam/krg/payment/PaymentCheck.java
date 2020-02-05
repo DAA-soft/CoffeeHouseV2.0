9cbe6834a6a509da414a9779183ee4c936371a69
Darkhan Sadykov <38325407+Khandora@users.noreply.github.com>
2020-02-05 18:59:02 +0600

    cycle update

--- src/com/epam/krg/payment/PaymentCheck.java
+++ src/com/epam/krg/payment/PaymentCheck.java
@@ -3,31 +3,37 @@ package com.epam.krg.payment;
 import java.util.Scanner;
 
 public class PaymentCheck {
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     protected void checkCard(int pinCode){
-        for ( ; ; ) {
+        while (pinCode >= 0) {
             if (pinCode < 10000 && pinCode > 999) {
                 break;
             } else {
                 System.out.println("Enter the valid pin-code.Enter '0' to quit");
                 pinCode = scanner.nextInt();
-                if(pinCode == 0) break;
+                if(pinCode == 0) {
+                    break;
+                } else {
+                    checkCard(pinCode);
+                }
             }
         }
     }
 
     protected void checkCash(int amountOfMoney, int costOfCoffee){
-        for ( ; ; ) {
+        while (costOfCoffee > 0 && amountOfMoney >= 0) {
             if (amountOfMoney < costOfCoffee) {
                 System.out.println("You need to pay " + (costOfCoffee - amountOfMoney) + " more USD!");
                 costOfCoffee -= amountOfMoney;
                 amountOfMoney = scanner.nextInt();
             } else if (amountOfMoney > costOfCoffee) {
+                costOfCoffee -= amountOfMoney;
                 System.out.println("Here's your change! " + (amountOfMoney - costOfCoffee) + " USD");
                 System.out.println("Enjoy your coffee!");
                 break;
             } else {
+                costOfCoffee -= amountOfMoney;
                 System.out.println("Here's your coffee! Enjoy");
                 break;
             }
